acd1e5390fd960d4934ab60f819873c4401f78e0
Mergim Hasani <mergimhasani@Mac.fritz.box>
2026-06-11 14:17:57 +0200

    përditësimi i kodit

--- js/scripts.js
+++ js/scripts.js
@@ -7,65 +7,65 @@
 // Scripts
 // 
 
-window.addEventListener('DOMContentLoaded', event => {
+// window.addEventListener('DOMContentLoaded', event => {
 
-    // Navbar shrink function
-    var navbarShrink = function () {
-        const navbarCollapsible = document.body.querySelector('#mainNav');
-        if (!navbarCollapsible) {
-            return;
-        }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
-        } else {
-            navbarCollapsible.classList.add('navbar-shrink')
-        }
+//     // Navbar shrink function
+//     var navbarShrink = function () {
+//         const navbarCollapsible = document.body.querySelector('#mainNav');
+//         if (!navbarCollapsible) {
+//             return;
+//         }
+//         if (window.scrollY === 0) {
+//             navbarCollapsible.classList.remove('navbar-shrink')
+//         } else {
+//             navbarCollapsible.classList.add('navbar-shrink')
+//         }
 
-    };
+//     };
 
-    // Shrink the navbar 
-    navbarShrink();
+//     // Shrink the navbar 
+//     navbarShrink();
 
-    // Shrink the navbar when page is scrolled
-    document.addEventListener('scroll', navbarShrink);
+//     // Shrink the navbar when page is scrolled
+//     document.addEventListener('scroll', navbarShrink);
 
-    //  Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            rootMargin: '0px 0px -40%',
-        });
-    };
+//     //  Activate Bootstrap scrollspy on the main nav element
+//     const mainNav = document.body.querySelector('#mainNav');
+//     if (mainNav) {
+//         new bootstrap.ScrollSpy(document.body, {
+//             target: '#mainNav',
+//             rootMargin: '0px 0px -40%',
+//         });
+//     };
 
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
-    });
-document.addEventListener("contextmenu", function(e) {
-  e.preventDefault();
-});
-document.addEventListener("keydown", function(e) {
-  if (
-    e.ctrlKey && (e.key === "c" || e.key === "s" || e.key === "u") ||
-    e.key === "F12"
-  ) {
-    e.preventDefault();
-  }
-});
-document.addEventListener("visibilitychange", function() {
-  if (document.hidden) {
-    document.body.style.filter = "blur(10px)";
-  } else {
-    document.body.style.filter = "none";
-  }
-});
-});
+//     // Collapse responsive navbar when toggler is visible
+//     const navbarToggler = document.body.querySelector('.navbar-toggler');
+//     const responsiveNavItems = [].slice.call(
+//         document.querySelectorAll('#navbarResponsive .nav-link')
+//     );
+//     responsiveNavItems.map(function (responsiveNavItem) {
+//         responsiveNavItem.addEventListener('click', () => {
+//             if (window.getComputedStyle(navbarToggler).display !== 'none') {
+//                 navbarToggler.click();
+//             }
+//         });
+//     });
+// document.addEventListener("contextmenu", function(e) {
+//   e.preventDefault();
+// });
+// document.addEventListener("keydown", function(e) {
+//   if (
+//     e.ctrlKey && (e.key === "c" || e.key === "s" || e.key === "u") ||
+//     e.key === "F12"
+//   ) {
+//     e.preventDefault();
+//   }
+// });
+// document.addEventListener("visibilitychange", function() {
+//   if (document.hidden) {
+//     document.body.style.filter = "blur(10px)";
+//   } else {
+//     document.body.style.filter = "none";
+//   }
+// });
+// });
